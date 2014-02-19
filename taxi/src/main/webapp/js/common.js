dec547e9f90ba0be602d4ffbe854a5abcc820910
S.O.S <S.O.S@SOS-PC>
2014-02-19 17:27:02 +0900

    2014-02-19   장종혁    myInfo 로컬스토리지 변경

--- taxi/src/main/webapp/js/common.js
+++ taxi/src/main/webapp/js/common.js
@@ -615,3 +615,37 @@ var push = {
     });
 
 })();
+
+
+/**
+ * 설  명: Local Storage MyInfo 저장
+ * 작성자: 장종혁
+ */
+var setLocalMyInfo = function(value){
+	  if (localStorage.myInfo)
+	    {
+		  localStorage.setItem("myInfo", JSON.stringify( value ));
+	    }
+	  else // 로컬스토리지에 myInfo가 없을때
+	    {
+			localStorage.setItem("myInfo", JSON.stringify( value ));
+	    }
+};
+
+
+/**
+ *  설  명 :  LocalStorage MyInfo 값 가져오기
+ *  작성자: 장종혁
+ */
+function getLocalMyInfo(){
+	return JSON.parse(localStorage.getItem("myInfo"));
+};
+
+
+/**
+*  설  명 : LocalStorage MyInfo 값 지우기
+* 작성자 ; 장종혁
+*/
+var delLocalMyInfo = function(){
+	localStorage.removeItem("myInfo");
+};
