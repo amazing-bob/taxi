8472895687158b8c0cb75b315f4320a4c981bdba
Buru <Buru@Haapy>
2014-03-10 20:30:52 +0900

    2014-03-10 	김상헌 	DB컬럼 변경 * MBR 테이블 MBR_DIVICE_NO  ---->  MBR_UUID
    * ROOM_MBR 테이블
    REF_FRND_NAME 제거

--- taxi/src/main/java/com/taxi/vo/member/Mbr.java
+++ taxi/src/main/java/com/taxi/vo/member/Mbr.java
@@ -17,7 +17,7 @@ public class Mbr extends BaseMbr implements Serializable {
 	 * 
 	 * protected int		mbrNo;
 	 * protected String 	mbrPhoneNo;
-	 * protected String 	mbrDeviceNo;
+	 * protected String 	mbrUuid;
 	 * protected String 	mbrName;
 	 * protected String 	mbrPhotoUrl;
 	 * protected String 	mbrGender;
@@ -35,8 +35,8 @@ public class Mbr extends BaseMbr implements Serializable {
 	public Mbr setMbrPhoneNo(String mbrPhoneNo) {
 		return (Mbr) super.setMbrPhoneNo(mbrPhoneNo);
 	}
-	public Mbr setMbrDeviceNo(String mbrDeviceNo) {
-		return (Mbr) super.setMbrDeviceNo(mbrDeviceNo);
+	public Mbr setMbrUuid(String mbrUuid) {
+		return (Mbr) super.setMbrUuid(mbrUuid);
 	}
 	public Mbr setMbrName(String mbrName) {
 		return (Mbr) super.setMbrName(mbrName);
@@ -56,69 +56,5 @@ public class Mbr extends BaseMbr implements Serializable {
 	public Mbr setMbrUpdateDate(Date mbrUpdateDate) {
 		return (Mbr) super.setMbrUpdateDate(mbrUpdateDate);
 	}
-	
-
-/*	//====================== AS-IS =======================//
- 	
-	protected String			mbrId;
-	protected String 		mbrName;
-	protected String 		mbrPhoneNo;
-	protected String 		mbrPhotoUrl;
-	protected String 		mbrGender;
-	protected Date 			mbrRegDate;
-	protected List<Frnd>	frndList;
-	
-	public List<Frnd> getFrndList() {
-		return frndList;
-	}
-	public Mbr setFrndList(List<Frnd> frndList) {
-		this.frndList = frndList;
-		return this;
-	}
-	
-	public String getMbrId() {
-		return mbrId;
-	}
-	public Mbr setMbrId(String mbrId) {
-		this.mbrId = mbrId;
-		return this;
-	}
-	public String getMbrName() {
-		return mbrName;
-	}
-	public Mbr setMbrName(String mbrName) {
-		this.mbrName = mbrName;
-		return this;
-	}
-	public String getMbrPhoneNo() {
-		return mbrPhoneNo;
-	}
-	public Mbr setMbrPhoneNo(String mbrPhoneNo) {
-		this.mbrPhoneNo = mbrPhoneNo;
-		return this;
-	}
-	public String getMbrPhotoUrl() {
-		return mbrPhotoUrl;
-	}
-	public Mbr setMbrPhotoUrl(String mbrPhotoUrl) {
-		this.mbrPhotoUrl = mbrPhotoUrl;
-		return this;
-	}
-	public String getMbrGender() {
-		return mbrGender;
-	}
-	public Mbr setMbrGender(String mbrGender) {
-		this.mbrGender = mbrGender;
-		return this;
-	}
-	@JsonSerialize(using = CustomDateSerializer.class)
-	public Date getMbrRegDate() {
-		return mbrRegDate;
-	}
-	public Mbr setMbrRegDate(Date mbrRegDate) {
-		this.mbrRegDate = mbrRegDate;
-		return this;
-	}
-*/
 	
 }
